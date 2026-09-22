53b7458dad2b699aa15b3a89b17e28116c5627fb
sydelise <sydelise2029@gmail.com>
2026-09-22 08:17:25 -0500

    added a new song

--- playlist.py
+++ playlist.py
@@ -4,6 +4,7 @@ SONGS = [
     ("Imagine", "John Lennon"),
     ("Blinding Lights", "The Weeknd"),
     ("Viva La Vida", "Coldplay"),
+    ("Distant Lover","Marvin Gaye")
 ]
 
 def print_playlist(songs):
